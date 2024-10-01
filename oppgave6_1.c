c9cbf92e070af54280514c44744ae56a38f54e89
Stefan Strand <stefanboytchev@hotmail.com>
2024-10-01 23:19:14 +0200

    completed task 1b oblig6

--- oppgave6_1.c
+++ oppgave6_1.c
@@ -3,29 +3,43 @@
 
 int count = 0;
 
+pthread_mutex_t lock;
+
 void *increase()
 {
     int i;
-    for(i = 0; i < 1e8; ++i)
+
+    for(i = 0; i < 1e8; ++i) {
+        pthread_mutex_lock(&lock);
         count++;
+        pthread_mutex_unlock(&lock);
+    }
 }
 
 void *decrease()
 {
     int i;
-    for(i = 0; i < 1e8; ++i)
+
+    for(i = 0; i < 1e8; ++i) {
+        pthread_mutex_lock(&lock);
         count--;
+        pthread_mutex_unlock(&lock);
+    }
 }
 
 int main()
 {
     pthread_t thread1, thread2;
 
+    pthread_mutex_init(&lock, NULL);
+
     pthread_create(&thread1, NULL, increase, NULL);
     pthread_create(&thread2, NULL, decrease, NULL);
 
     pthread_join(thread1, NULL);
     pthread_join(thread2, NULL);
+
+    pthread_mutex_destroy(&lock);
 
     printf("count = %d\n", count);
 }
