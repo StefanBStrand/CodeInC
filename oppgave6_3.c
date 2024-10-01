a3dbd21bfb84f848c38606fc2a48227c09d58b82
Stefan Strand <stefanboytchev@hotmail.com>
2024-10-02 01:45:03 +0200

    added task 3 oblig 6 OS

---
+++ oppgave6_3.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+
+#define MAX_WAIT 2000000
+
+pthread_mutex_t mutex;
+pthread_cond_t cond;
+int blocked_threads = 0;
+
+void *terningkast(void *nr)
+{
+    while(1)
+    {
+        int terning = rand() % 6 + 1;
+
+        pthread_mutex_lock(&mutex);
+        if (terning == 1) {
+            blocked_threads++;
+            printf("Tråd %3ld -> %d (%d)\n", (long) nr, terning, blocked_threads);
+            pthread_cond_wait(&cond, &mutex);  // Wait for signal
+            blocked_threads--;  // Once unblocked, decrement the count
+        }
+        else if (terning == 6) {
+            pthread_cond_signal(&cond);  // Signal one waiting thread
+            printf("Tråd %3ld -> %d (%d)\n", (long) nr, terning, blocked_threads);
+        }
+        else {
+            printf("Tråd %3ld -> %d\n", (long) nr, terning);  // Regular output
+        }
+
+        pthread_mutex_unlock(&mutex);
+
+        usleep(rand() % MAX_WAIT + 1);  // Random delay
+    }
+}
+
+int main()
+{
+    int n;
+    long i;
+    pthread_t t;
+
+    printf("n? ");
+    scanf("%d", &n);
+
+    pthread_mutex_init(&mutex, NULL);
+    pthread_cond_init(&cond, NULL);
+
+    for (i = 0; i < n; i++)
+        pthread_create(&t, NULL, terningkast, (void *) i);
+
+    pthread_join(t, NULL);  // Wait for threads (infinite loop, so it never really finishes)
+
+}
